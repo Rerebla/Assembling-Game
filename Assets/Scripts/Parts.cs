d6d65eb2efb56cf69d5f38de5aaa53fe89091211
Rerebla <52045338+Rerebla@users.noreply.github.com>
2020-07-31 18:45:59 +0200

    Add basic collision detection for Game Objects

---
+++ Assets/Scripts/Parts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour {
+    public bool isBuyable;
+    public int price;
+    public bool isBasePart;
+    public bool isFinalPart;
+    // public List<GameObject> snapZones = new List<GameObject>();
+    public string ID;
+
+}
---
+++ Assets/Scripts/SnapZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapZone : MonoBehaviour {
+    public List<string> acceptableIDs = new List<string>();
+    private void OnTriggerEnter(Collider otherCollider) {
+        Parts parts = otherCollider.GetComponent<Parts>();
+        if (parts == null) {
+            return;
+        }
+        if (acceptableIDs.Contains(parts.ID)) {
+            string snappedID = parts.ID;
+            //TODO: Delete the GO and make a new one at the snap zones center.
+        }
+    }
+}
